752f871b76bb8921b96d31849c2f3f38c2ca053f
ThanhMai1109 <maivietthanh213@gmail.com>
2025-10-04 23:27:35 +0700

    HUAHUS

--- fashionshop.cpp
+++ fashionshop.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-// --------------------Product----------------------
+// --------------------Product---------------------- uiiiaihuirwhctghiegt
 class Products{
     private:
     string productID;
